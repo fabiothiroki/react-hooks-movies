9056576a43dc2f837ca5db38e947c86f66cafcce
Fabio Hiroki <fabiothiroki@gmail.com>
2020-05-21 17:27:29 -0300

    update layout

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <h2><strong>All Games<span>( 4 )</span></strong></h2>
+      <div class="cards">
+        <figure class="card">
+          <img src="https://mrreiha.keybase.pub/codepen/hover-fx/1.jpg" />
+          <figcaption>Dota 2</figcaption>
+        </figure>
+
+        <figure class="card">
+          <img src="https://mrreiha.keybase.pub/codepen/hover-fx/2.jpg" />
+          <figcaption>Stick Fight</figcaption>
+        </figure>
+
+        <figure class="card">
+          <img src="https://mrreiha.keybase.pub/codepen/hover-fx/3.jpg" />
+          <figcaption>Minion Masters</figcaption>
+        </figure>
+
+        <figure class="card">
+          <img src="https://mrreiha.keybase.pub/codepen/hover-fx/4.jpg" />
+          <figcaption>KoseBoz!</figcaption>
+        </figure>
+
+      </div>
     </div>
   );
 }
